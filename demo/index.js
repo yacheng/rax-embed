431318a0b33c5a24384a0bdb29169a0795d6a243
泠苞 <liaoshuai.ls@alibaba-inc.com>
2019-07-29 17:24:59 +0800

    style

--- demo/index.js
+++ demo/index.js
@@ -1,14 +1,15 @@
-import {createElement, render, Component} from 'rax';
+/* eslint-disable import/no-extraneous-dependencies */
+import { createElement, render, Component } from 'rax';
 import * as DriverDOM from 'driver-dom';
 import * as DriverWeex from 'driver-weex';
 import { isWeex } from 'universal-env';
 import Embed from '../src/index';
 
 const urlParam = {
-  paramOne:123,
-  paramTwo:456
+  paramOne: 123,
+  paramTwo: 456
 };
 render(<Embed urlParam={urlParam} src={'http://taobao.com'} useIframeInWeb={true} style={{
   height: 750,
   width: 750
-}} />, document.body, { driver:isWeex ? DriverWeex : DriverDOM });
+}} />, document.body, { driver: isWeex ? DriverWeex : DriverDOM });
